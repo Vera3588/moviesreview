a51e107a980f1ce9425865aa2f12cdf81fde2bb8
Vera3588 <javeraa@eafit.edu.co>
2022-02-10 17:31:23 -0500

    version2

--- moviesreviews/movie/views.py
+++ moviesreviews/movie/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1> Welcome to home page </h1>')
+    return HttpResponse('<h1> Welcome to home page, Juan Vera </h1>')
 # Create your views here.
